d1812ce86b4febb21c2e9f680ab5dfdd5b2f3565
Andile <andilenzmbatha0@gmail.com>
2024-07-17 21:18:21 +0200

    Added search /API intergration and date

--- src/app.js
+++ src/app.js
@@ -1,9 +1,55 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector(".weather-app-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "cd7ff0b39233e546tfae64f37ao44b9a";
+  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("form");
 searchFormElement.addEventListener("submit", search);
+searchCity("Osizweni");
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+let currentDateELement = document.querySelector(".app-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
