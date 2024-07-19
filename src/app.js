1df6cfae55f4fc47e555299e58c5180fd36b5b6c
Andile <andilenzmbatha0@gmail.com>
2024-07-19 09:06:00 +0200

    Added forecast

--- src/app.js
+++ src/app.js
@@ -62,3 +62,26 @@ let currentDateELement = document.querySelector(".app-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+function updateForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Tues", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+   <div class="weather-forecast-date">${day}</div>
+   <div class="weather-forecast-icon">🌤️</div>
+   <div class="weather-forecast-temperatures">
+     <div class="weather-forecast-temperature">
+       <strong>15º</strong>
+     </div>
+     <div class="weather-forecast-temperature">9º</div>
+   </div>
+ </div>`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+updateForecast();
